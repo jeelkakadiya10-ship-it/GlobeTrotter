acefed02adf744651a5f7337d8f0834842509135
jeelkakadiya10-ship-it <jeelkakadiya@users.noreply.github.com>
2026-08-22 16:03:35 +0530

    Remove files except README

--- server/src/routes/shareRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getPublicTripBySlug } from '../controllers/shareController.js';
-
-const router = Router();
-
-// Completely public endpoint - zero auth required
-router.get('/:slug', getPublicTripBySlug);
-
-export default router;
